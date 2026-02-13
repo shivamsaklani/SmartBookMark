b298a059e1e8b466e45910a548fef476a0dddf9b
shivasaklani1@gmail.com <shivasaklani1@gmail.com>
2026-02-13 23:56:46 +0530

    prisma_init

--- prisma/migrations/20260213182628_init/migration.sql
+++ prisma/migrations/20260213182628_init/migration.sql
@@ -11,7 +11,7 @@ CREATE TABLE "Profile" (
 
 -- CreateTable
 CREATE TABLE "Collection" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "userId" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "icon" TEXT,
@@ -23,15 +23,13 @@ CREATE TABLE "Collection" (
 
 -- CreateTable
 CREATE TABLE "Bookmark" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "userId" UUID NOT NULL,
     "collectionId" UUID,
     "url" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
-    "favicon" TEXT,
     "isFavorite" BOOLEAN NOT NULL DEFAULT false,
-    "visits" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("id")
@@ -39,7 +37,7 @@ CREATE TABLE "Bookmark" (
 
 -- CreateTable
 CREATE TABLE "Tag" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
 
     CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
@@ -47,20 +45,36 @@ CREATE TABLE "Tag" (
 
 -- CreateTable
 CREATE TABLE "BookmarkTag" (
-    "bookmarkId" TEXT NOT NULL,
-    "tagId" TEXT NOT NULL,
+    "bookmarkId" UUID NOT NULL,
+    "tagId" UUID NOT NULL,
 
     CONSTRAINT "BookmarkTag_pkey" PRIMARY KEY ("bookmarkId","tagId")
+);
+
+-- CreateTable
+CREATE TABLE "_CollectionToProfile" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL,
+
+    CONSTRAINT "_CollectionToProfile_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_BookmarkToProfile" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL,
+
+    CONSTRAINT "_BookmarkToProfile_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
 
--- AddForeignKey
-ALTER TABLE "Collection" ADD CONSTRAINT "Collection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "_CollectionToProfile_B_index" ON "_CollectionToProfile"("B");
 
--- AddForeignKey
-ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "_BookmarkToProfile_B_index" ON "_BookmarkToProfile"("B");
 
 -- AddForeignKey
 ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -70,3 +84,15 @@ ALTER TABLE "BookmarkTag" ADD CONSTRAINT "BookmarkTag_bookmarkId_fkey" FOREIGN K
 
 -- AddForeignKey
 ALTER TABLE "BookmarkTag" ADD CONSTRAINT "BookmarkTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CollectionToProfile" ADD CONSTRAINT "_CollectionToProfile_A_fkey" FOREIGN KEY ("A") REFERENCES "Collection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CollectionToProfile" ADD CONSTRAINT "_CollectionToProfile_B_fkey" FOREIGN KEY ("B") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BookmarkToProfile" ADD CONSTRAINT "_BookmarkToProfile_A_fkey" FOREIGN KEY ("A") REFERENCES "Bookmark"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BookmarkToProfile" ADD CONSTRAINT "_BookmarkToProfile_B_fkey" FOREIGN KEY ("B") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
