98dc58f7b2b40ab153b2782fcada42ba830c4722
shivasaklani1@gmail.com <shivasaklani1@gmail.com>
2026-02-15 12:16:43 +0530

    minorchange

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -20,7 +20,6 @@ export function AppHeader({ onAddBookmark }: AppHeaderProps) {
       <SidebarTrigger />
 
       <div className="relative flex-1 max-w-md">
-        <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
         <Input
           placeholder="Search bookmarks..."
           value={searchQuery}
